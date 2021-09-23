cf85bfd0b0497dfdb70730d17a72926ef2871cc9
nikbit <85894877+nikbit@users.noreply.github.com>
2021-09-23 18:06:17 +0200

    aggiunte le operazioni effettuate

--- main.ts
+++ main.ts
@@ -8,9 +8,11 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(den)
 })
 input.onButtonPressed(Button.AB, function () {
+    basic.showString("Decimale x 100 =")
     basic.showNumber(num / den * 100)
     basic.showString("%")
 })
 input.onButtonPressed(Button.B, function () {
+    basic.showString("numer. : denom. =")
     basic.showNumber(num / den)
 })
